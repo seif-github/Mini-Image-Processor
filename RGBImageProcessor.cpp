9c8ba048e119a6cb9362410c843a11fec29125c7
Zeyad Hussein <zeyad.hussein404@gmail.com>
2023-10-16 17:24:25 +0300

    add 3 6 9 c f filters

--- RGBImageProcessor.cpp
+++ RGBImageProcessor.cpp
@@ -74,11 +74,30 @@ void black_and_white() {
 
 /* 2- Invert Filter-----------------------------------------------------*/
 void invert_image(){
-    
+
 }
 
 /* 3- Merge Filter------------------------------------------------------*/
 void merge_image(){
+    //Declare a matrix and array of characters for the second image
+    unsigned char input_image_matrix_2 [SIZE][SIZE][RGB];
+    char input_image_name_2 [100];
+
+    // Get gray scale image file name
+    cout << "Please enter name of image file to merge with: " ;
+    cin >> input_image_name_2;
+    // Add to it .bmp extension and load image to the image matrix
+    strcat (input_image_name_2, ".bmp");
+    readRGBBMP (input_image_name_2, input_image_matrix_2);
+
+    for (int i = 0 ; i < SIZE; i++){
+        for (int j = 0 ; j < SIZE; j++ ){
+            for (int k = 0; k < RGB; k++){
+                //Make every pixel in output matrix equals to the average of two input matrices.
+                output_image_matrix [i][j][k] = (input_image_matrix [i][j][k] + input_image_matrix_2 [i][j][k]) / 2 ;
+            }
+        }
+    }
 
 }
 
@@ -112,11 +131,53 @@ void flip_image(){
 
 /* 5- Darken and Lighten Image------------------------------------------*/
 void darken_and_lighten(){
-    
+
 }
 
 /* 6- Rotate Image------------------------------------------------------*/
 void rotate_image(){
+
+     // Declare an integer to choose
+    cout << "Rotate (90), (180), (270) or (360) degrees? ";
+    int degree;
+    cin >> degree;
+    // Change place of every pixel in its position in the output matrix
+    if (degree == 90){
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++) {
+                    output_image_matrix [i][j][k] = input_image_matrix [255-j][i][k];
+                }
+            }
+        }
+    }
+    else if(degree == 180){
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++) {
+                    output_image_matrix [i][j][k] = input_image_matrix [255-i][j][k];
+                }
+            }
+        }
+    }
+    else if (degree == 270){
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++) {
+                    output_image_matrix [i][j][k] = input_image_matrix [j][255-i][k];
+                }
+            }
+        }
+    }
+    else{ // degree == 360
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++) {
+                    output_image_matrix [i][j][k] = input_image_matrix [i][j][k];
+                }
+            }
+        }
+    }
 
 }
 
@@ -154,11 +215,55 @@ void detect_edges(){
 
 /* 8- Enlarge Image-----------------------------------------------------*/
 void enlarge_image(){
-    
+
 }
 /* 9- Shrink Image------------------------------------------------------*/
 void shrink_image(){
-    
+
+    // Declare string to choose
+    cout<<"Shrink to (1/2), (1/3) or (1/4)? ";
+    string value ;
+    cin>> value ;
+    // Make the whole image white
+    for(int i = 0; i < SIZE;i++){
+        for(int j = 0; j < SIZE; j++){
+            for (int k = 0; k < RGB; k++){
+                output_image_matrix[i][j][k] = 255;
+            }
+        }
+    }
+
+    if(value == "1/2"){
+        // Shrink image by taking every 4 pixels as one
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++){
+                    output_image_matrix[i/2][j/2][k] = input_image_matrix[i][j][k];
+                }
+
+            }
+        }
+    }
+    else if(value == "1/3") {
+        // Shrink image by taking every 9 pixels as one
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++){
+                output_image_matrix[i/3][j/3][k] = input_image_matrix[i][j][k];
+                }
+            }
+        }
+    }
+    else{ // value == "1/4"
+        // Shrink image by taking every 16 pixels as one
+        for(int i = 0; i < SIZE;i++){
+            for(int j = 0; j < SIZE; j++){
+                for (int k = 0; k < RGB; k++){
+                    output_image_matrix[i/4][j/4][k] = input_image_matrix[i][j][k] ;
+                }
+            }
+        }
+    }
 }
 /* a- Mirror Image------------------------------------------------------*/
 void mirror_image(){
@@ -214,12 +319,27 @@ void mirror_image(){
 }
 /* b- Shuffle Image-----------------------------------------------------*/
 void shuffle_image(){
-    
+
 }
 
 /* c- Blur Image--------------------------------------------------------*/
 void blur_image(){
-    
+
+        // Declare an integer to calculate the average of a pixel and the surrounding 24 pixel and make it one pixel
+    int avg = 0;
+    for(int i = 2; i < 254;i++){
+        for(int j = 2; j < 254; j++){
+            for (int k = 0; k < RGB; k++){
+                avg += input_image_matrix[i][j][k];
+                for(int l = 1; l < 4; l++){
+                    avg += input_image_matrix[i-l][j-l][k] + input_image_matrix[i-l][j][k] + input_image_matrix[i-l][j+l][k] + input_image_matrix[i][j-l][k]+ input_image_matrix[i][j+l][k] + input_image_matrix[i+l][j-l][k] + input_image_matrix[i+l][j][k] + input_image_matrix[i+l][j+l][k];
+                }
+                output_image_matrix[i][j][k] = avg/25;
+                avg = 0;
+            }
+        }
+    }
+
 }
 
 /* d- Crop Image--------------------------------------------------------*/
@@ -248,12 +368,50 @@ void crop_image(){
 
 /* e- Skew Image Right--------------------------------------------------*/
 void skew_right(){
-    
+
 }
 
 /* f- Skew Image Up-----------------------------------------------------*/
 void skew_up(){
-    
+
+       // Declare a number to determine the angle
+    cout<<"Please enter degree to skew up: ";
+    double angle;
+    cin >> angle;
+    angle = 90 - angle; // To work on the right angle
+    angle = (angle*22)/(180*7); // Convert angle from degree to radian
+    double expand = 256 / tan(angle); // Distance calculated from the tangent ratio; tan(angle) = opposite / adjacent
+    double copy_expand = expand; // Take a copy to use it in shrink for loop
+    double shrink_ratio = round((256+expand)/256); // Calculate the ratio fit the shrink on 256*256
+    double step = expand / SIZE; // Calculate the value of every step to skew the photo
+    unsigned char expanded_matrix[SIZE+(int)expand][SIZE][RGB]; // Declare a matrix to fit the expand after skewing
+    // Make the output matrix and the expanded matrix white
+    for(int i = 0; i < SIZE ;i++){
+        for(int j = 0; j < SIZE+(int)expand; j++){
+            for (int k = 0; k < RGB; k++){
+                output_image_matrix[i][j][k] = 255 ;
+                expanded_matrix[j][i][k] = 255;
+            }
+        }
+    }
+    // Skewing the image by start from distance (expand) and subtracting by value (step) until finish the skewed image
+    for(int i = 0; i < SIZE ;i++){
+        for(int j = 0; j < SIZE; j++){
+            for (int k = 0; k < RGB; k++){
+                expanded_matrix[j+(int)expand][i][k] = input_image_matrix[j][i][k];
+            }
+        }
+        expand -= step;
+    }
+    // Shrink image by taking every value of (shrink_ratio) pixels as one pixel
+    for(int i = 0; i < SIZE ;i++){
+        for(int j = 0; j < SIZE+(int)copy_expand; j++){
+            for (int k = 0; k < RGB; k++){
+                output_image_matrix[j/(int)shrink_ratio][i][k] = expanded_matrix[j][i][k];
+            }
+        }
+    }
+
 }
 
 /*======================================================================*/
